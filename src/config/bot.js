af22c45998d6ae09dcbf7ff60b52f27a7ab41ac5
greenguy8 <grimace.filtch@gmail.com>
2026-05-14 14:51:38 -0500

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Living it up in the hot tub with my soulmate👌",
+        name: "seeing stars here bruh",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
